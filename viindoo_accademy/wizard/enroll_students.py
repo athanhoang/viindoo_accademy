b438fe215dba607bed1d6131b346c2b81d64a9f1
daththp <dat.hoang@viindoo.com>
2022-12-19 08:55:04 +0700

    [WIP] Usable for student model

--- viindoo_accademy/wizard/enroll_students.py
+++ viindoo_accademy/wizard/enroll_students.py
@@ -1,20 +1,33 @@
-from odoo import models,fields
+from odoo import models,fields, api
 
 class EnrollStudents(models.TransientModel):
     _name = 'enroll.students'
     _description = 'Enroll Students'
     
     class_ids = fields.Many2many('education.class', string='Class')
-    student_ids = fields.Many2many('education.student', required=True)
+    student_ids = fields.Many2many('education.student', string = 'Students')
     enrollment_date = fields.Datetime(string = "Enrollment Date", required=True)
+    is_class_model = fields.Boolean(default=False,  compute = '_compute_is_class_model')
+    
+    @api.depends('class_ids')
+    def _compute_is_class_model(self):
+        active_model = self.env.context.get('active_model')
+        if active_model == 'education.class':
+            return True
 
     def action_enroll_student_apply(self):
-        active_model = self.env.context.get('active_model')
-        class_ids = self.env[active_model].browse(self.env.context.get('active_ids'))
         vals_list = []
-        
+        active_model = self.env.context.get('active_model')
+
+        if active_model == 'education.class':
+            class_ids = self.env[active_model].browse(self.env.context.get('active_ids'))
+            student_ids = self.student_ids
+        elif active_model == 'education.student':
+            class_ids = self.class_ids
+            student_ids = self.env[active_model].browse(self.env.context.get('active_ids'))
+
         for c in class_ids:
-            for s in self.student_ids:
+            for s in student_ids:
                 vals = {
                     'enrollment_date': self.enrollment_date,
                     'class_id': c.id,
